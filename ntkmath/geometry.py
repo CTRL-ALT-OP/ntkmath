29426e11b2ee808ce55c14774a2fc1db0467ac5d
CTRL-ALT-OP <ctrl.alt.op@gmail.com>
2025-06-19 23:30:15 -0600

    Test version 2

--- ntkmath/geometry.py
+++ ntkmath/geometry.py
@@ -1,18 +1,34 @@
+# Subscriptable point class
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
 class Rectangle:
-    rotation = 0
-    x = 0
-    y = 0
-    width = 0
-    height = 0
+    def __init__(self, x=0, y=0, width=0, height=0, rotation=0):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.rotation = rotation
+
+    def point_in_rectangle(self, x, y):
+        pass
 
 
 class Triangle:
-    rotation = 0
-    x = 0
-    y = 0
-    width = 0
-    height = 0
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
 
 
+# Static class
 class Angle:
+    pass
+
+
+def Line(angle, length):
+    """Return a point from the end of a line from angle and length."""
     pass
